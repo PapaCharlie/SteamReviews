a000c31acc32e2ae7e134d20213df6b6a84647d5
Paul Chesnais <paul.chesnais@gmail.com>
2017-04-28 17:34:02 -0400

    UI (#8)
    * Restructured frontend a bit, added more info
    
    * Fix tags JSON
    
    So that we don't have to ghetto parse a __str__ of a Python dict
    
    * Added UI for tag filtering
    
    * Added game redirect button functionality
    
    * added api abilities / bias vector scaled by 0.3
    
    * changed config and init for environ, switched from using sessions to cookies
    
    * The joy of UI
    
    * added username as part of the steam api
    
    * fixed users controller
    
    * submit button correctly posting
    
    * Reworked spider graph
    
    * Small UI change to prep for merge
    
    * CoMMiT t0 m3rG3
    
    * Added tag cloud

---
+++ app/static/js/search.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+	$(".details-close").click(function() {
+		$(".details-content-wrapper").fadeOut(100, function() {
+			$(".details-overlay").animate({width:'0px'}, 300);
+		});
+	});
+
+	$(".details-query").click(function() {
+		window.location.replace("/?app_id=" + selectedAppID);
+	});
+
+	$(".result-box").click(function() {
+		var resultBox = $(this);
+		$(".details-title").text(resultBox.data("title"));
+		$(".details-img").attr("src", resultBox.children(".result-img").attr("src"));
+		$(".details-link").attr("href", resultBox.data("steam-url"));
+
+		var tags = resultBox.data("tags");
+		var tags_keys = [];
+		var tags_values = [];
+		for (var key in tags) {
+	    if (!tags.hasOwnProperty(key)) continue;
+	    tags_keys.push(key);
+	    tags_values.push(tags[key]);
+	  }
+		var data = {
+		    labels: tags_keys,
+		    datasets: [
+		        {
+		            label: "Game tags",
+		            backgroundColor: "rgba(255,99,132,0.2)",
+		            borderColor: "rgba(255,99,132,1)",
+		            pointBackgroundColor: "rgba(255,99,132,1)",
+		            pointBorderColor: "#fff",
+		            pointHoverBackgroundColor: "#fff",
+		            pointHoverBorderColor: "rgba(255,99,132,1)",
+		            data: tags_values
+		        }
+		    ]
+		};
+
+		var attributeChart = $(".details-attribute-chart");
+		var myRadarChart = new Chart(attributeChart, {
+			type: 'radar',
+		    data: data,
+		    options: {
+		    	legend: {
+		    		display: false
+		    	},
+		    	scale: {
+			        ticks: {
+			            display: false
+			        }
+			    },
+			    tooltips: {
+			    	enabled: false
+			    }
+		    }
+		});
+
+		$(".details-overlay").animate({width:'40%'}, 300, function() {
+			$(".details-content-wrapper").fadeIn(100);
+		});
+	});
+});
